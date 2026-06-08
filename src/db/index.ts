381cce76f10262d519f44881df1ef3ee0ec1424f
bharat roy <bharatroy1005@gmail.com>
2026-06-08 18:08:14 +0600

    users table is created

--- src/db/index.ts
+++ src/db/index.ts
@@ -7,7 +7,18 @@ export const pool = new Pool({
 export const initDB = async () => {
     try {
         await pool.query(`
-            
+            CREATE TABLE IF NOT EXISTS users(
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(20),
+            email VARCHAR(20) UNIQUE NOT NULL,
+            password TEXT NOT NULL,
+            role VARCHAR(20) 
+            CHECK(role IN('contributor', 'maintainer'))
+            DEFAULT ('contributor'),
+
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            )
     `)
     } catch (error) {
 
