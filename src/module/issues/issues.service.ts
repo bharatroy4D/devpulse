45cf27f8ba7a583a3846b38172cbcd77b46f2996
bharat roy <bharatroy1005@gmail.com>
2026-06-14 15:16:50 +0600

    getSingle issues of reporter_id=user.id added

--- src/module/issues/issues.service.ts
+++ src/module/issues/issues.service.ts
@@ -15,10 +15,31 @@ const createIssuesIntoDB = async (payload: any, reporterId: number) => {
 
 };
 const getSingleIssuesIntoDB = async (id: string) => {
-    const result = await pool.query(`
-    SELECT * FROM issues WHERE id=$1
+    // const result = await pool.query(`
+    // SELECT * FROM issues WHERE id=$1
 
-    `, [id])
+    // `, [id])
+    // return result;
+    const result = await pool.query(`
+  SELECT
+    i.id,
+    i.title,
+    i.description,
+    i.type,
+    i.status,
+    i.created_at,
+    i.updated_at,
+    json_build_object(
+      'id', u.id,
+      'name', u.name,
+      'role', u.role
+    ) AS reporter
+  FROM issues i
+  JOIN users u
+    ON i.reporter_id = u.id
+  WHERE i.id = $1
+  `, [id]);
+
     return result;
 }
 export const issuesService = {
