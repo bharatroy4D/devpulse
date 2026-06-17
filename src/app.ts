0c66c511f140c5723d24c0d01f49b91ed808ab4a
bharat roy <bharatroy1005@gmail.com>
2026-06-17 13:13:24 +0600

    glabal error handler set

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,11 @@ import express, { type Application, type Request, type Response } from "express"
 import { issuesRoute } from "./module/issues/issues.route";
 import { authRoute } from "./module/auth/auth.route";
 import cors from "cors"
+import glabalErrorHandler from "./middleware/globalErrorHandle";
 const app: Application = express();
 app.use(express.json());
 app.use(cors({
     origin: 'http://localhost:5000',
-
 }))
 
 app.get('/', (req: Request, res: Response) => {
@@ -14,5 +14,6 @@ app.get('/', (req: Request, res: Response) => {
 })
 app.use('/api/auth', authRoute);
 app.use('/api/issues', issuesRoute)
+app.use(glabalErrorHandler)
 
 export default app;
